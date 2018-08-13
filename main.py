48407a4ee3054376d1519fed9750cc55e77da946
Jade Kinomoto <jadekinomoto@Jades-MacBook-Pro.local>
2018-08-13 11:03:43 -0700

    signup

--- main.py
+++ main.py
@@ -11,9 +11,16 @@ jinja_env = jinja2.Environment(
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write("Welcome to Study Spot")
-
+class SignupHandler(webapp2.RequestHandler):
+    def get(self):
+        Signup_template = jinja_current_directory.get_template(
+            "templates/signup.html")
+        self.response.write(Signup_template.render())
 
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/signup', SignupHandler),
+    ], debug=True)
+    
