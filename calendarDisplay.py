5accfbd87926575db879d0a06fe9d4180e248979
Megan <talbertmegan@gmail.com>
2018-08-14 16:07:28 -0700

    cleaned up duplicates of code

--- calendarDisplay.py
+++ calendarDisplay.py
@@ -1,10 +1,10 @@
 from index import events
 from data import Course
 
-def filterEvents:
-    for course in courses:
-        course = querySelector.Course(name)
-        userEvents = []
-        if "{{course}}" in event.summary:
-            userEvents.append(event)
+def filterEvents(course_names, events):
+    user_events = []
+    for course in course_names:
+        for event in events:
+            if course in event.summary:
+                userEvents.append(event)
     return userEvents
