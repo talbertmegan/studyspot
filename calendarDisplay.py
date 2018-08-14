fb76c51dcdeaa755c26bfc1632a50372325fe333
Megan <talbertmegan@gmail.com>
2018-08-14 15:09:43 -0700

    Trying to get the calendar to work

---
+++ calendarDisplay.py
@@ -0,0 +1,10 @@
+from index import events
+from data import Course
+
+def filterEvents:
+    for course in courses:
+        course = querySelector.Course(name)
+        userEvents = []
+        if "{{course}}" in event.summary:
+            userEvents.append(event)
+    return userEvents
